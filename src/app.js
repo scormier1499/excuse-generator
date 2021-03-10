ac7fb577e2113d0ef277e6b5fa2efc077793eb64
scormier1499 <sheila_cormier@hotmail.com>
2021-03-10 02:26:15 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["My Pokemon", "My snake", "My Kid", "My Neighbor"];
+  let action = ["captured", "swallowed", "shredded", "broke"];
+  let what = ["monitor", "instructions", "the car"];
+  let when = [
+    "before the presentation",
+    "right on before arrival",
+    "when I finished",
+    "during my conference",
+    "while I was on my way"
+  ];
+
+  let sentence =
+    who[getRndInteger(0, who.length - 1)] +
+    " " +
+    action[getRndInteger(0, action.length - 1)] +
+    " " +
+    what[getRndInteger(0, what.length - 1)] +
+    " " +
+    when[getRndInteger(0, when.length - 1)] +
+    " ";
+
+  let target = document.querySelector("#excuse");
+
+  target.innerHTML = sentence;
+
+  console.log(getRndInteger);
 };
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
